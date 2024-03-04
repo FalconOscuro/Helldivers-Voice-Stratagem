29c3116052b788db343abe75b36d40911d01dbd8
Peter Garrod <p.jlgarrod@gmail.com>
2024-03-04 16:38:33 +0000

    Basic stratagem configuration

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+from PyQt5.QtWidgets import QWidget
 
 import speech_recognition as sr
 import keyboard as kb
@@ -17,45 +18,77 @@ class Status(enum.Enum):
     PROCESSING = { "colour" : Qt.red, "text" : "Processing..." }
     EXECUTING = { "colour" : Qt.green, "text" : "Executing!" }
 
-class status_box(QLineEdit):
-    def __init__(self, *args, **kwargs):
-        QLineEdit.__init__(self, *args, **kwargs)
+def format_command(command):
+    return command.replace("-", " ").lower()
 
-        self.setReadOnly(True)
-    
+class StatusGroup(QGroupBox):
+    def __init__(self, parent=None):
+        super(QGroupBox, self).__init__(parent)
+        self.setTitle("Status")
+
+        self.status = QLineEdit()
+        self.status.setReadOnly(True)
+
+        self.outbox = QPlainTextEdit()
+        self.outbox.setReadOnly(True)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.status)
+        layout.addWidget(self.outbox)
+        self.setLayout(layout)
+
     def setStatus(self, status: Status):
         palette = QPalette()
         palette.setColor(QPalette.Text, status.value["colour"])
-        self.setPalette(palette)
-        self.setText(status.value["text"])
+        self.status.setPalette(palette)
+        self.status.setText(status.value["text"])
+    
+    def print(self, text: str):
+        self.outbox.insertPlainText(text + '\n')
 
-class output_box(QPlainTextEdit):
-    def __init__(self, *args, **kwargs):
-        QPlainTextEdit.__init__(self, *args, **kwargs)
+class StratagemOpt(QWidget):
+    def __init__(self, stratagem, parent=None):
+        super(QWidget, self).__init__(parent)
 
-        self.setReadOnly(True)
+        self.stratagem = stratagem
+        name = QLabel(stratagem["name"])
 
-def format_command(command):
-    return command.replace("-", " ").lower()
+        toggle = QCheckBox("Enabled")
+        toggle.setChecked(stratagem["enabled"])
+        toggle.toggled.connect(self.changeEnabled)
+
+        layout = QVBoxLayout()
+        layout.addWidget(name)
+        layout.addWidget(toggle)
+        self.setLayout(layout)
+    
+    def changeEnabled(self, enabled: bool):
+        self.stratagem["enabled"] = enabled
+
+
+class StratagemGroup(QGroupBox):
+    def __init__(self, stratagems, parent=None):
+        super(QGroupBox, self).__init__(parent)
+        self.setTitle("Stratagems")
+        self.stratagems = stratagems
+
+        scroll_layout = QVBoxLayout()
+        for stratagem in stratagems:
+            scroll_layout.addWidget(StratagemOpt(stratagem))
+        
+        sw = QWidget()
+        sw.setLayout(scroll_layout)
+
+        scrollArea = QScrollArea()
+        scrollArea.setWidget(sw)
+
+        layout = QVBoxLayout()
+        layout.addWidget(scrollArea)
+        self.setLayout(layout)
 
 class hdvs(QMainWindow):
     def __init__(self, parent=None):
         super(QMainWindow, self).__init__(parent)
-
-        self.setWindowTitle("Helldivers Voice Stratagem")
-        layout = QVBoxLayout()
-
-        status = status_box()
-        layout.addWidget(status)
-        self.status = status
-
-        outbox = output_box()
-        layout.addWidget(outbox)
-        self.outbox = outbox
-
-        main = QWidget()
-        main.setLayout(layout)
-        self.setCentralWidget(main)
 
         print("Starting stratagem recogition")
         print("SpeechRecognition Version: {0}".format(sr.__version__))
@@ -79,12 +112,24 @@ class hdvs(QMainWindow):
 
         print("Ready")
 
+        self.setWindowTitle("Helldivers Voice Stratagem")
+
+        self.status = StatusGroup()
+        self.stratagemopts = StratagemGroup(self.stratagems)
+
+        mainLayout = QHBoxLayout()
+        mainLayout.addWidget(self.stratagemopts)
+        mainLayout.addWidget(self.status)
+
+        main = QWidget()
+        main.setLayout(mainLayout)
+        self.setCentralWidget(main)
         self.status.setStatus(Status.IDLE)
 
     def execute_stratagem(self, stratagem):
         self.status.setStatus(Status.EXECUTING)
         config = self.config
-        self.myprint("Executing {0}.".format(stratagem["name"]))
+        self.status.print("Executing {0}.".format(stratagem["name"]))
         for key in stratagem["code"]:
             if key == "U":
                 kb.send(config["keys"]["up"])
@@ -110,7 +155,7 @@ class hdvs(QMainWindow):
             # Has not been done in yaml file for readability
             for trigger in stratagem["trigger"]:
                 if command == format_command(trigger):
-                    self.myprint("Found match: {0}".format(stratagem["name"]))
+                    self.status.print("Found match: {0}".format(stratagem["name"]))
                     self.execute_stratagem(stratagem)
                     return True
         return False
@@ -121,34 +166,31 @@ class hdvs(QMainWindow):
 
         while True:
             status.setStatus(Status.LISTENING)
-            self.myprint("Listening...")
+            self.status.print("Listening...")
             with self.mic as source:
                 audio = recog.listen(source)
-    
-            self.myprint("Input recieved, converting... ")
+
+            self.status.print("Input recieved, converting... ")
             status.setStatus(Status.PROCESSING)
             try:
                 command = recog.recognize_google(audio)
-                self.myprint("Heard: {0}".format(command))
+                self.status.print("Heard: {0}".format(command))
 
                 if self.interpret_stratagem(command):
                     break
                 else:
-                    self.myprint("{0} was not a valid stratagem.".format(command))
+                    self.status.print("{0} was not a valid stratagem.".format(command))
 
             except sr.UnknownValueError:
-                self.myprint("Could not understand audio.")
+                self.status.print("Could not understand audio.")
             except sr.RequestError as e:
-                self.myprint("Could not decode, recieved error: {0}".format(e))
-        
+                self.status.print("Could not decode, recieved error: {0}".format(e))
+
             if not kb.is_pressed(self.strat_key):    # Break if strategem key no longer held
-                self.myprint("Stratagem key not pressed, listening stopped.")
+                self.status.print("Stratagem key not pressed, listening stopped.")
                 break
-        
+
         status.setStatus(Status.IDLE)
-    
-    def myprint(self, text: str):
-        self.outbox.insertPlainText(text + "\n")
     
     def exec(self, stratagem_file, config_file):
         self.app.exec_()
