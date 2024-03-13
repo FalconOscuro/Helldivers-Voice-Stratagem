4df01a50902de3ebf71d9d23c5d3e8452d523900
Peter Garrod <p.jlgarrod@gmail.com>
2024-03-13 14:47:20 +0000

    Stratagem data structure

---
+++ include/HDVS/stratagem.h
@@ -0,0 +1,74 @@
+#ifndef STRATAGEM_H
+#define STRATAGEM_H
+
+#include <string>
+#include <vector>
+#include <yaml-cpp/yaml.h>
+
+namespace hdvs {
+
+struct Stratagem
+{
+    std::string name;
+    std::string icon;
+    std::vector<std::string> trigger;
+    bool enabled;
+
+    enum Direction {
+        UP = 'U',
+        DOWN = 'D',
+        LEFT = 'L',
+        RIGHT = 'R',
+    }; // enum Direction
+    std::vector<Direction> code;
+
+    enum Type {
+        NONE        = 0      ,
+        SPECIAL     = 1      ,
+        HELLPOD     = 1 << 1 ,
+        EXPLOSIVE   = 1 << 2 ,
+        MISSION     = 1 << 3 ,
+        WEAPON      = 1 << 4 ,
+        EAGLE       = 1 << 5 ,
+        SUPPLY      = 1 << 6 ,
+        INCENDIARY  = 1 << 7 ,
+        OFFENSIVE   = 1 << 8 ,
+        ORBIRTAL    = 1 << 9 ,
+        BACKPACK    = 1 << 10,
+        DEFENSIVE   = 1 << 11,
+        SENTRY      = 1 << 12,
+        VEHICLE     = 1 << 13,
+    }; // enum Type
+    static const char NUM_TYPES = 14;
+
+    Type type;
+
+    std::string GetIcon() const;
+    bool operator==(const Stratagem& rhs) const;
+}; // struct Stratagem
+
+} // namespace hdvs
+
+namespace YAML {
+
+template<>
+struct convert<hdvs::Stratagem> {
+    static Node encode(const hdvs::Stratagem& strat);
+    static bool decode(const Node& node, hdvs::Stratagem& strat);
+}; // struct convert<hdvs::Stratagem>
+
+template<>
+struct convert<hdvs::Stratagem::Type> {
+    static Node encode(const hdvs::Stratagem::Type& strat);
+    static bool decode(const Node& node, hdvs::Stratagem::Type& strat);
+}; // struct convert<hdvs::Stratagem::Type>
+
+//template<>
+//struct convert<hdvs::Stratagem::Direction> {
+//    static Node encode(const hdvs::Stratagem::Direction& strat);
+//    static bool decode(const Node& node, hdvs::Stratagem::Direction& strat);
+//}; // struct convert<hdvs::Stratagem::Direction>
+
+} // namespace YAML
+
+#endif // STRATAGEM_H
---
+++ src/stratagem.cpp
@@ -0,0 +1,89 @@
+#include "HDVS/stratagem.h"
+
+namespace hdvs {
+
+bool Stratagem::operator==(const Stratagem& rhs) const {
+    // Compare tirggers? Un-ordered so == not viable
+   return name == rhs.name && code == rhs.code && type == rhs.type; 
+}
+
+// Ordered array of Stratagem Types as seen in enum for conversion
+const std::string TYPE_NAMES[Stratagem::NUM_TYPES] = {
+    "special"       , "hellpod" , "explosive"   , "mission"     ,
+    "support weapon", "eagle"   , "supply"      , "incendiary"  ,
+    "offensive"     , "orbital" , "backpack"    , "defensive"   ,
+    "sentry"        , "vehicle"
+};
+
+} // namespace hdvs
+
+namespace YAML {
+
+Node convert<hdvs::Stratagem>::encode(const hdvs::Stratagem& strat)
+{
+    Node node;
+
+    node["name"] = strat.name;
+    node["trigger"] = strat.trigger;
+    node["code"] = strat.code;
+    node["type"] = strat.type;
+    node["enabled"] = strat.enabled;
+
+    if (strat.icon != "")
+        node["icon"] = strat.icon;
+
+    return node;
+}
+
+bool convert<hdvs::Stratagem>::decode(const Node& node, hdvs::Stratagem& strat)
+{
+    if (!(node.IsMap() && node["name"] && node["trigger"] && node["code"] && node["type"]))
+        return false;
+
+    strat.name = node["name"].as<std::string>();
+    strat.enabled = node["enabled"] ? node["enabled"].as<bool>() : false;
+    strat.icon = node["icon"] ? node["icon"].as<std::string>() : "";
+
+    // big and block of remaining conversions
+    return (
+        convert<std::vector<std::string>>::decode(node["trigger"], strat.trigger)
+            && convert<std::vector<hdvs::Stratagem::Direction>>::decode(node["code"], strat.code)
+            && convert<hdvs::Stratagem::Type>::decode(node["type"], strat.type)
+    );
+}
+
+// this is overly complex, could've just used vector
+Node convert<hdvs::Stratagem::Type>::encode(const hdvs::Stratagem::Type& type)
+{
+    Node node;
+
+    // check flags individually, if existing, add to sequence
+    for (char i = 0; i < hdvs::Stratagem::NUM_TYPES; i++)
+        if ((1 << i) & type)
+            node.push_back(hdvs::TYPE_NAMES[i]);
+
+    return node;
+}
+
+// this is overly complex, could've just used vector
+bool convert<hdvs::Stratagem::Type>::decode(const Node& node, hdvs::Stratagem::Type& type)
+{
+    if (!node.IsSequence())
+        return false;
+
+    for (auto i = 0; i < node.size(); i++)
+    {
+        std::string typeStr = node[i].as<std::string>();
+        for (auto j = 0; j < hdvs::Stratagem::NUM_TYPES; j++)
+            if (hdvs::TYPE_NAMES[j] == typeStr) // loop all type names
+            {
+                type = (hdvs::Stratagem::Type)(type | 1 << j);
+                break;
+                // error/warn if fail?
+            }
+    }
+
+    return true;
+}
+
+} // namespace YAML
